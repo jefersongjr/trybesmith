0b3234c5dacd9a38fc71e2161369daf45866111d
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-06 17:44:00 -0300

    feat: chamando rota no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import productsRouter from './routes/ProductsRouter';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(productsRouter);
 
 export default app;
