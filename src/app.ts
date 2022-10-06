a50ff72fa2e305ad97cbab023c6f5fde2e5a5eee
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-06 19:06:16 -0300

    feat: chamando o userRouter no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productsRouter from './routes/ProductsRouter';
+import userRouter from './routes/UserRouter';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(productsRouter);
+app.use(userRouter);
 
 export default app;
