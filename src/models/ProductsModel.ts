256bef28ceee2aa7509d6a179bf9e85ae1022813
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-06 16:11:50 -0300

    feat: Criando model User e função createProducts

---
+++ src/models/ProductsModel.ts
@@ -0,0 +1,24 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IProducts } from "../interfaces";
+import connection from "./connection";
+
+export default class ProductsModel {
+  private connection : Pool;
+
+  constructor()  {
+    this.connection = connection;
+  }
+
+  public createProduct = async(products: IProducts): Promise<IProducts> { 
+        const { name, amount } = products;
+
+        const query = 'INSERT INTO Products (name, amout) VALUES (?, ?)';
+        const values = [name, amount];
+
+        const [result] = await this.connection.execute<ResultSetHeader>(query, values);
+        const { insertId: id } = result;
+        
+        const created: IProducts = { id, ...products }
+        return created;
+  }
+}
