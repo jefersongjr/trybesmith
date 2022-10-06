a99b73682fe6abfce613af8dfecae9d22052b961
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-06 18:11:42 -0300

    feat: criando rota get products

--- src/routes/ProductsRouter.ts
+++ src/routes/ProductsRouter.ts
@@ -6,5 +6,6 @@ const productsRouter = Router();
 const controller = new ProductsController();
 
 productsRouter.post('/products', controller.createNewProduct);
+productsRouter.get('/products', controller.getAllProducts);
 
 export default productsRouter;
