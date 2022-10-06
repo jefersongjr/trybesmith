306b7d77e7935e4e1e7e334866ddfbb4c51fea94
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-06 16:15:14 -0300

    fix: corrigindo erros do Lint

--- src/models/ProductsModel.ts
+++ src/models/ProductsModel.ts
@@ -1,24 +1,25 @@
 import { Pool, ResultSetHeader } from 'mysql2/promise';
-import { IProducts } from "../interfaces";
-import connection from "./connection";
+import { IProducts } from '../interfaces';
+import connection from './connection';
 
 export default class ProductsModel {
   private connection : Pool;
 
-  constructor()  {
+  constructor() {
     this.connection = connection;
   }
 
-  public createProduct = async(products: IProducts): Promise<IProducts> { 
-        const { name, amount } = products;
+  public createProduct = async (products: IProducts): Promise<IProducts> => { 
+    const { name, amount } = products;
 
-        const query = 'INSERT INTO Products (name, amout) VALUES (?, ?)';
-        const values = [name, amount];
+    const query = 'INSERT INTO Products (name, amout) VALUES (?, ?)';
+    const values = [name, amount];
 
-        const [result] = await this.connection.execute<ResultSetHeader>(query, values);
-        const { insertId: id } = result;
+    const [result] = await this.connection.execute<ResultSetHeader>(query, values);
+    const { insertId: id } = result;
         
-        const created: IProducts = { id, ...products }
-        return created;
-  }
+    const created: IProducts = { id, ...products };
+  
+    return created;
+  };
 }
