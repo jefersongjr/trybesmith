3fbe15e54f714fd9441e3d8d86ffb763c3757b8e
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-06 20:37:00 -0300

    fix : consertando userService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,5 +1,12 @@
+import jwt from 'jsonwebtoken';
 import { IUser } from '../interfaces';
 import UserModel from '../models/UserModel';
+
+const generateToken = async (user: IUser): Promise<string> => {
+  const payload = { id: user.id, user: user.username };
+  const token = jwt.sign(payload, 'secret');
+  return token;
+};
 
 export default class UserService {
   private model: UserModel;
@@ -8,8 +15,14 @@ export default class UserService {
     this.model = new UserModel();
   }
 
-  public createUser = async (user: IUser): Promise<IUser> => {
-    const created = await this.model.newUser(user);
-    return created;
+  public createUser = async ( 
+    username: string,
+    classe: string, 
+    level: number, 
+    password: string,
+  ): Promise<string> => {
+    const user = await this.model.newUser({ username, classe, level, password });
+    const token = await generateToken(user);
+    return token;
   };
 }
