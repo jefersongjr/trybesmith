5f6537eeaf6102b89f0831365056c161450c29f1
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-07 01:21:01 -0300

    feat: criando a camada model e função getAll para listar todas as orders

---
+++ src/models/OrdersModel.ts
@@ -0,0 +1,22 @@
+import { Pool } from 'mysql2/promise';
+import { IOrders } from '../interfaces';
+import connection from './connection';
+
+export default class OrdersModel {
+  private connection: Pool;
+    
+  constructor() {
+    this.connection = connection;
+  }
+
+  public findOrders = async (): Promise<IOrders[]> => {
+    const query = `SELECT  orderId AS id, userId,
+                  JSON_ARRAYAGG(products.id) AS productId
+                  FROM Trybesmith.Products AS products 
+                  INNER JOIN Trybesmith.Orders AS orders ON products.orderId = orders.id
+                  GROUP BY orderId;`;
+    const [result] = await this.connection.execute(query);
+
+    return result as IOrders[];
+  };
+}
